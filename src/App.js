f5e29ba75ad058ecebd1ee973fad37b5bb2ce77a
footios <footios76@gmail.com>
2019-01-26 11:14:16 +0200

    183. Preparing the Project For Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import Blog from "./containers/Blog/Blog";
+
+// This is now the router off this application
+// and now we can use routing features from that routing
+// package anywhere inside this
+// wrapping components
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+
+import Post from "../../../components/Post/Post";
+import "./Posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const udatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: udatedPosts });
+      })
+      // udate the state incase smth went wrong
+      .catch(error => {
+        //console.log(error);
+        this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler(id) {
+    this.setState({ selectedPostId: id });
+  }
+
+  render() {
+    let posts = (
+      <p style={{ textAlign: "center", color: "red" }}>Something went wrong!</p>
+    );
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      ));
+    }
+
+    return (
+      /* Now I want to use routing in here.
+    'section posts' should be a custom component 
+    which we load when we visit /nothing on the URL. */
+
+      <div>
+        <section className="Posts">{posts}</section>
+      </div>
+    );
+  }
+}
+
+export default Posts;
