2ace07dc7adaa2f436184d222b5a287d40d01121
footios <footios76@gmail.com>
2019-01-28 09:09:16 +0200

    191. Apsolute vs Relative path

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,37 @@ import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 
+// An absolute path is always appended to your domain,
+// so if you're serving this app from example.com
+// then you want to go to new-post,
+// if you navigate to /new-post, that simply means
+// always attach /new-post right after the domain.
+// Even if you previously were at example.com/posts,
+// you wouldn't add new-post at the end of that, turning
+// it to posts/new-post but turning it into example.com/new-post and omitting posts,
+// this is what an absolute path does.
+
+// But if you want to create a Relative path
+// you need to build a link which simply appends the path to the end of the current path,
+// use this approach: pathname: this.props.match.url + "/new-post",
+
+// Absolute vs Relative Paths (Article)
+// Section 11, Lecture 191
+
+// You learned about <Link> , you learned about the to  property it uses.
+// The path you can use in to can be either absolute or relative.
+// Absolute Paths
+// By default, if you just enter to="/some-path"  or to="some-path" , that's an absolute path.
+// Absolute path means that it's always appended right after your domain. Therefore, both syntaxes (with and without leading slash) lead to example.com/some-path .
+
+// Relative Paths
+// Sometimes, you might want to create a relative path instead. This is especially useful, if your component is already loaded given a specific path (e.g. posts ) and you then want to append something to that existing path (so that you, for example, get /posts/new ).
+// If you're on a component loaded via /posts , to="new"  would lead to example.com/new , NOT example.com/posts/new .
+// To change this behavior, you have to find out which path you're on and add the new fragment to that existing path. You can do that with the url  property of props.match :
+// <Link to={props.match.url + '/new'}>  will lead to example.com/posts/new  when placing this link in a component loaded on /posts . If you'd use the same <Link>  in a component loaded via /all-posts , the link would point to /all-posts/new .
+// There's no better or worse way of creating Link paths - choose the one you need. Sometimes, you want to ensure that you always load the same path, no matter on which path you already are => Use absolute paths in this scenario.
+// Use relative paths if you want to navigate relative to your existing path.
+
 class Blog extends Component {
   render() {
     return (
