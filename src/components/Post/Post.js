f1f508edf2b6490c6b485f02d61c37b6a323cb25
footios <footios76@gmail.com>
2019-01-28 08:52:45 +0200

    189. Make component Route aware using 'withRouter' (hoc)

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./Post.css";
 
-const post = props => (
-  <article className="Post" onClick={props.clicked}>
-    <h1>{props.title}</h1>
-    <div className="Info">
-      <div className="Author">{props.author}</div>
-    </div>
-  </article>
-);
+const post = props => {
+  // Use withRouter (hoc) to get the props of the Router
+  // to make the component Route aware
+  // Now they have: history, location and match props
+  console.log(props);
+  return (
+    <article className="Post" onClick={props.clicked}>
+      <h1>{props.title}</h1>
+      <div className="Info">
+        <div className="Author">{props.author}</div>
+      </div>
+    </article>
+  );
+};
 
-export default post;
+export default withRouter(post);
