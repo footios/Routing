aa73acbe34904675e686c855b9b042c7df8a0c71
footios <footios76@gmail.com>
2019-02-01 10:34:40 +0200

    206. Loading Routes Lazily with hoc asyncComponent

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,12 +4,49 @@ import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+// When ever you are importing something...,
+// you basically inform 'webpack',
+// (the build tool which gets used behind the scenes)
+// about this dependency and it will include it in the global bundle.
+// But we don't want that for lazy loading.
+// Still 'webpack' needs to be able to dynamically prepare
+// some extra bundle for this potentially loaded code.
+
+//import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
+const AsyncNewPost = asyncComponent(() => {
+  // This (return import(...)) is a special syntax.
+  // It's the 'dynamic import syntax' which means
+  // whatever comes between the parentheses, is only imported
+  // when that function here is executed.
+  // And that function here will only be executed once we render
+  // AsyncNewPost to the screen.
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   };
+  // 1.
+  // Now, let's go to the 'network' tab in the developer tools.
+  // There if I go into posts you see that we're loading this bundle.js file.
+  // This contains all our source code, and it's rather big.
+  // So loading the entire bundle with all the code of our application up front
+  // can be bad if we have a big application with distinct features
+  // and distinct areas in the app where a user might never visit a certain area.
+  // E.g. in our app, loading NewPost could be redundant.
+  // It should only be loaded if the user actually navigates to NewPost.
+  // So why should we download the code up front?
+  // The technique of downloading only what you need is known as
+  // 'code splitting' or 'lazy loading'.
+
+  // To implement 'code splitting' or 'lazy loading',
+  // we need 'create react app' and 'react route for',
+  // because 'code splitting' depends heavily
+  // on the 'webpack' configuration you are using.
+
+  // So for this to work in this setup, we need a higher order component.
 
   componentDidMount = () => {
     console.log("this.props in Blog", this.props);
@@ -46,27 +83,14 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        {/* You can specify the route component here and if you leave out the 'path' 
-        so you don't add 'path', you can simply define a component
-        or the render method, to render something for any unknown route.
-        So here I'm going to output 'not found'.
-        Of course you could use α component and render a 404 error page component.
-        But this route will catch any routes which are not handled prior to it.*/}
+        {/* */}
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" component={NewPost} />
+            <Route path="/new-post" component={AsyncNewPost} />
           ) : null}
           <Route path="/posts/" component={Posts} />
           <Route render={() => <h1>Not found</h1>} />
-          {/* 
-          Now as I said, it won't work together with 'Redirect', 
-          if you redirect from '/'. If you redirect from any
-          error page than slash, it'll work though.
-          Because 'from' is treated as a prefix and therefore it catches
-          all routes as does the 'Not found' 'Route' and therefore 
-          no matter how you order them, it won't work together.
-          
-          <Redirect from="/" to="/posts" /> */}
+          {/*<Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
     );
