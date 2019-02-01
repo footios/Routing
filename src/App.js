4b185b90da9cc7f94187ae3b97f55e65e56313aa
footios <footios76@gmail.com>
2019-02-01 11:12:34 +0200

    Intitial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-import Blog from "./containers/Blog/Blog";
-
-// This is now the router off this application
-// and now we can use routing features from that routing
-// package anywhere inside this
-// wrapping components
+import Posts from "./containers/Posts";
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
+        <React.Fragment>
+          <nav>
+            <NavLink to="/user">User Page</NavLink> |&nbsp;
+            <NavLink to="/posts">Posts Page</NavLink>
+          </nav>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/user" component={User} />
+          <Route path="/posts" component={Posts} />
+        </React.Fragment>
       </BrowserRouter>
     );
   }
